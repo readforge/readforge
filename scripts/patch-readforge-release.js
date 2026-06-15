e05e645bbf2a9e0cf62c37518ecdd678a1bdf714
readforge <billywwaire@gmail.com>
2026-06-14 21:51:45 -0400

    Use safe release patch only

--- scripts/patch-readforge-release.js
+++ scripts/patch-readforge-release.js
@@ -1,11 +1,8 @@
 const fs = require('fs')
 const path = require('path')
-
 const indexPath = path.join(process.cwd(), 'index.html')
 const version = process.env.RELEASE_VERSION || '1.7.21'
-
 let index = fs.readFileSync(indexPath, 'utf8')
 index = index.replace(/ReadForge v\d+\.\d+\.\d+/g, `ReadForge v${version}`)
-
 fs.writeFileSync(indexPath, index, 'utf8')
-console.log('Build patch applied safely: version badge only.')
+console.log('Safe release patch complete.')
