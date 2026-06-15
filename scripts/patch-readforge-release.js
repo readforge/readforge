9acc93c979969bdc6e8007ad66e33066c0516d76
readforge <billywwaire@gmail.com>
2026-06-14 22:11:56 -0400

    Trigger stable build after patch restore

--- scripts/patch-readforge-release.js
+++ scripts/patch-readforge-release.js
@@ -8,4 +8,4 @@ let index = fs.readFileSync(indexPath, 'utf8')
 index = index.replace(/ReadForge v\d+\.\d+\.\d+/g, `ReadForge v${version}`)
 
 fs.writeFileSync(indexPath, index, 'utf8')
-console.log('Build patch applied: version badge only. Reader tools are built into React now.')
+console.log('Stable reader build patch.')
